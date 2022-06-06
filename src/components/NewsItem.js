af41ae41918dde33688d048ce447bac155d65469
Muskan <muskan80525@gmail.com>
2022-06-06 17:01:16 +0530

    feat: Infinite scroll, top loading, apikey as env variable

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,8 +5,11 @@ export class NewsItem extends Component {
         let { title, description, imageUrl, url, author, date, source } = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{ width: '18rem' }}>
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '85%'}}>{source}</span>
+                <div className="card">
+                    <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0'}}> 
+                    <span className="badge rounded-pill bg-danger" style={{left: '85%'}}>{source}</span>
+
+                    </div>
                     <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
